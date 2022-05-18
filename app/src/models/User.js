eda951a20e94b98b678f0368c10e8b445dd547c0
JoSeongYoon <whtjddbs0112@naver.com>
2022-05-18 13:54:31 +0900

    add css, html good

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -15,9 +15,9 @@ class User {
             if (id === body.id && passwd === body.passwd){
                 return {success: true};
             }
-            return {success: false, msg: "not password"};
+            return {success: false, msg: "패스워드가 일치하지 않습니다."};
         }
-        return {success: false, msg: "not id"};
+        return {success: false, msg: "아이디가 일치하지 않습니다."};
     }
 }
 
